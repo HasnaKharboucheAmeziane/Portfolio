0fe5a48f3384f20f3be813fd00468847b57365b9
HasnaKharboucheAmeziane <hasna.kharboucheameziane@latoile.em-lyon.com>
2025-04-15 23:34:45 +0200

    JS popover

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const popover = document.getElementById("monpopover");
 const toggleBtn = document.getElementById("toggleBtn");
 
+const toggleBtn2 = document.getElementById("toggleBtn2");
+const popover2 = document.getElementById("monpopover2");
+
 // Vérifie la prise en charge de l'API Popover.
 function supportsPopover() {
   return HTMLElement.prototype.hasOwnProperty("popover");
@@ -14,7 +17,16 @@ if (supportsPopover()) {
 
   // Définit que le bouton peut afficher/masquer le popover
   toggleBtn.popoverTargetAction = "toggle";
+
+  popover2.popover = "auto";
+  toggleBtn2.popoverTargetElement = popover2;
+  toggleBtn2.popoverTargetAction = "toggle";
+
 } else {
   popover.textContent = "L'API Popover n'est pas prise en charge.";
   toggleBtn.hidden = true;
+
+  popover2.textContent = "L'API Popover n'est pas prise en charge.";
+  toggleBtn2.hidden = true;
 }
+
