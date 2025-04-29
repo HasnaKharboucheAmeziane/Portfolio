9b4b370af56131d6f9bd97a2405d6d9c2a91d9d5
HasnaKharboucheAmeziane <hasna.kharboucheameziane@latoile.em-lyon.com>
2025-04-29 23:41:58 +0200

    menu burger ok

--- script.js
+++ script.js
@@ -33,3 +33,11 @@ if (supportsPopover()) {
 }
 
 
+
+  const burger = document.getElementById("burger");
+  const navLinks = document.getElementById("nav-links");
+
+  burger.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+  });
+
